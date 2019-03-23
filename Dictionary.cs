022a293d1f4e8d63288b60ddeac8fc3ddab6ac71
Grigoris Kokozides <40428198+Gregory95@users.noreply.github.com>
2019-03-23 22:36:25 +0200

    Add files via upload

---
+++ Dictionary.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Text;
+using System.IO;
+using System.Net.Mime;
+using Microsoft.Ajax.Utilities;
+using ReviewExplorer.Controllers;
+using ReviewExplorer.Models;
+
+namespace ReviewExplorer.Models
+{
+    public class Dictionary
+    {
+
+      
+    }
+
+
+}
---
+++ ReviewData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Text;
+using Microsoft.Office.Interop.Excel;
+using System.IO;
+using OfficeOpenXml;
+using ReviewExplorer.Controllers;
+using System.Data;
+using DataTable = System.Data.DataTable;
+
+namespace ReviewExplorer.Models
+{
+    public class ReviewData
+    {
+      [Required]
+      [StringLength(255)]
+      public string Text { get; set; }
+      
+      public List<string> OpinionList { get; set; }
+
+
+
+    }
+}
